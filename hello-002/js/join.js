2c853637cf76941b8885b7f061e7bb920b636dd8
hyojeong <mj01164@naver.com>
2024-07-10 11:03:45 +0900

    index

--- hello-002/js/join.js
+++ hello-002/js/join.js
@@ -9,33 +9,50 @@ document.addEventListener("DOMContentLoaded", () => {
   const input_name = join_body.querySelector("input[name='name']");
   const input_tel = join_body.querySelector("input[name='tel']");
   const btn_join = join_body.querySelector("input.join");
+
+  // 회원가입버튼(btn_join)이 클릭되었을때 실행할 함수
+  // 이 함수내에서 각 데이터의 유효성 검사를 실행한다
   const onValidation = () => {
     const value_username = input_username.value;
     const value_password = input_password.value;
     const value_re_password = input_re_password.value;
 
     if (!value_username) {
-      alert("사용자 ID는 반드시 입력해야 합니다");
+      alert("USERNAME은 반드시 입력해야 합니다");
       input_username.select();
       return false;
+    }
+    const userNameRexp = /^(?=.*[a-z0-9])[a-z0-9]{3,16}$/;
+    if (userNameRexp.test(value_username)) {
+      alert("USER NAME 3 ~ 16자리 이내의 영문, 숫자 조합으로 입력하세요");
     }
     if (!value_password) {
       alert("비밀번호는 반드시 입력해야 합니다");
       input_password.select();
       return false;
     }
+    const passwordRexp =
+      /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+    if (!passwordRexp.test(value_password)) {
+      alert(
+        "비밀번호는 영문, 숫자, 특수문자 포함 8 ~ 15자리 규칙으로 입력하세요"
+      );
+      input_password.select();
+      return false;
+    }
     if (!value_re_password) {
-      alert("비밀번호확인은 반드시 입력해야 합니다");
+      alert("비밀번호 확인은 반드시 입력해야 합니다");
       input_re_password.select();
       return false;
     }
+    // 비밀번호와 비밀번호 확인 다를 경우
     if (value_password !== value_re_password) {
-      alert("비밀번호와 비밀번호확인은 반드시 일치해야합니다");
+      alert("비밀번호와 비밀번호 확인은 반드시 일치해야합니다");
       input_re_password.select();
       return false;
     }
 
-    form.submit();
+    join_form.submit();
   };
   /*
   join(회원가입 버튼을 클릭했을 때)
